960c924c2a3e75a1fa6eb250e6dc964825959cc2
LoTwT <709937065@qq.com>
2021-12-02 00:20:11 +0800

    feat: sort directory

--- src/preload/preload.js
+++ src/preload/preload.js
@@ -1,5 +1,6 @@
 const { handle } = require("./channel.js")
 const fs = require("promise-fs")
+const path = require("path")
 
 handle(
   "readFile",
@@ -11,5 +12,22 @@ handle(
   async (filepath, content) => await fs.writeFile(filepath, content),
 )
 
-// 读取文件目录
-handle("readDir", async (dir) => await fs.readdir(dir))
+/**
+ * 读取文件目录
+ * 协助前端判断文件路径类型 (文件夹/文件)
+ */
+handle("readDir", async (dir) => {
+  let ret = []
+
+  const list = await fs.readdir(dir)
+  for (let i = 0; i < list.length; i++) {
+    const stat = await fs.stat(path.resolve(dir, list[i]))
+
+    ret.push({
+      name: list[i],
+      isDir: await stat.isDirectory(),
+    })
+  }
+
+  return ret
+})
